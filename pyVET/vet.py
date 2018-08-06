70ccd288e013456b6ef3c1fab19457f9fd7f26af
Andres Perez Hortal <andresperezcba@gmail.com>
2018-08-06 17:48:46 -0400

    Add support for masked arrays
    Remove mask input argument from vet.
    Add indexing option to transpose the output.

--- pyVET/vet.py
+++ pyVET/vet.py
@@ -28,6 +28,7 @@ from __future__ import (absolute_import, division,
 
 from numpy import float64
 import numpy
+from numpy.ma.core import MaskedArray
 from scipy.ndimage.interpolation import zoom
 from scipy.optimize._minimize import minimize
 from skimage.util.shape import view_as_blocks
@@ -46,7 +47,10 @@ def round_int(scalar):
 
 def morph(image, displacement):
     """
-    Morph image by applying a displacement field (Warping).
+    Morph image by applying a displacement field (Warping) in the images
+    coordinates.
+    
+    Take caution that (rows, columns)=(i,j)==(y,x).
 
     See :py:func:`vet._vet._morph`.
     """
@@ -88,8 +92,6 @@ def downsize(input_array, x_factor, y_factor=None):
     data = input_block_view.mean(-1).mean(-1)
 
     return numpy.ma.masked_invalid(data)
-
-
 
 
 def cost_function(sector_displacement_1d,
@@ -165,17 +167,18 @@ def cost_function(sector_displacement_1d,
         print()
         print("residuals", residuals)
         print("smoothness_penalty", smoothness_penalty)
-    return residuals + smoothness_penalty 
+    return residuals + smoothness_penalty
 
 # TODO: input parameter check
 # TODO: add keywords for minimization options
+# TODO: add masked arrays support! 
 def vet(input_images,
-        mask=None,
         factors=[32, 16, 4, 2, 1],
         smooth_gain=100,
         first_guess=None,
         intermediate_steps=False,
-        verbose=True):
+        verbose=True,
+        indexing='xy'):
     '''
     Variational Echo Tracking Algorithm presented in
     `Laroche and Zawadzki (1995)`_  and used in the McGill Algorithm for
@@ -208,39 +211,46 @@ def vet(input_images,
 
     Parameters
     ----------
-        
+
     input_images : ndarray_
         Input images, sequence of 2D arrays, or 3D arrays.
         The first dimension represents the template_image and the input_image.
-        The template_image (first element in first dimensions) denotes the 
+        The template_image (first element in first dimensions) denotes the
         reference image used to obtain the displacement (2D array).
         The second is the target image.
-        The expected dimensions are (2,nx,ny). 
+        The expected dimensions are (2,ny,nx).
+        Be aware the the 2D images dimensions correspond to (y, x).
 
-    factors : list or array
+    factors : list or array, optional
         If dimension is 1, the same factors will be used both image dimensions
         (x and y). If is 2D, the each row determines the factors of the
         corresponding dimensions.
         The factors denotes the number of sectors in each dimension used in
         each scaling procedure.
 
-    smooth_gain : float
+    smooth_gain : float, optional
         Smooth gain factor
 
-    first_guess : ndarray_
+    first_guess : ndarray, optional_
         If first_guess is not present zeros are used as first guess.
         The shape should be compatible with the input image.
         That is (2,nx,ny).
 
-    intermediate_steps : bool
+    intermediate_steps : bool, optional
         If True, also return a list with the first guesses obtained during the
         scaling procedure. False, by default.
 
 
-    verbose : bool
-        Verbosity enabled if True
+    verbose : bool, optional
+        Verbosity enabled if True (default). 
 
-
+    indexing : {‘xy’, ‘ij’}, optional
+        Cartesian (‘xy’, default) or matrix (‘ij’) indexing of output.
+        If ‘xy’ is selected, the displacement field dimensions correspond
+        to (x,y). Otherwise, the displacements corresponds to indexes of the
+        2D images (i,j).
+        
+    
     Returns
     -------
 
@@ -264,9 +274,12 @@ def vet(input_images,
     doi: 10.1175/1520-0493(2002)130<2859:SDOTPO>2.0.CO;2.
 
     '''
-    
+
     print("Running VET algorithm")
     
+    if indexing not in ['xy', 'ij']:
+        raise ValueError("Valid values for `indexing` are 'xy' and 'ij'.")
+            
     if verbose:
         def debug_print(*args, **kwargs):
             print(*args, **kwargs)
@@ -275,18 +288,30 @@ def vet(input_images,
             pass
 
     input_images = numpy.array(input_images, dtype=numpy.float64)
-    
+
     if (input_images.ndim != 3 or input_images.shape[0] != 2):
         raise FatalError("input_images dimension mismatch",
-                         "input_images.shape: %s"%str(input_images.shape),
+                         "input_images.shape: %s" % str(input_images.shape),
                          "(2, x, y ) dimensions expected.")
-        
-    template_image = numpy.array(input_images[0,:], dtype=numpy.float64)
-    input_image = numpy.array(input_images[1,:], dtype=numpy.float64)
+
+
+    # Get mask
+    if isinstance(input_images , MaskedArray):
+        mask = numpy.ma.getmaskarray(input_images)
+    else:
+        # Mask invalid data 
+        input_images = numpy.ma.masked_invalid(input_images)
+        mask = numpy.ma.getmaskarray(input_images)
     
-    if mask is None:
-        mask=numpy.zeros_like(template_image, dtype='int8')
-        
+    input_images[mask] = 0  # Remove any Nan from the raw data
+    
+    # Create a 2D mask with the rigth data type for _vet
+    mask = numpy.asarray(numpy.any(mask, axis=0), dtype='int8') 
+    
+    template_image = numpy.asarray(input_images.data[0, :], dtype=numpy.float64)
+    input_image = numpy.asarray(input_images.data[1, :], dtype=numpy.float64)
+
+
     # Check that the factors divide the domain
     factors = numpy.asarray(factors, dtype=numpy.int)
 
@@ -302,7 +327,7 @@ def vet(input_images,
             raise FatalError("Error computing VET",
                              "Incorrect factors dimensions.",
                              "factor should be a 1D if the input images"
-                             +" are square or 2D array otherwise")
+                             + " are square or 2D array otherwise")
 
     # Check that the factors divide the domain
     for i in range(factors.shape[1]):
@@ -338,6 +363,7 @@ def vet(input_images,
 
     if intermediate_steps:
         scaling_guesses = list()
+        
     for i in range(factors.shape[1]):
         # Minimize for each sector size
 
@@ -368,26 +394,31 @@ def vet(input_images,
                           options={'eps': 0.1, 'gtol': 0.1,
                                    'maxiter': 20, 'disp': True}
                           )
-        
-        
+
         first_guess = result.x.reshape(*(first_guess.shape))
-        
+
         if verbose:
             cost_function(result.x,
                           _template_image, _input_image,
                           factors[:, i], mask,
                           smooth_gain,
                           debug=True)
-            
+
         if first_guess is None:
-            scaling_guesses.append(first_guess)
+            if indexing == 'xy':
+                scaling_guesses.append(first_guess[::-1,:,:])
+            else:
+                scaling_guesses.append(first_guess)
 
     first_guess = zoom(first_guess,
                        (1,
                         _template_image.shape[0] / first_guess.shape[1],
                         _template_image.shape[1] / first_guess.shape[2]),
                        order=1, mode='nearest')
-
+    
+    if indexing == 'xy':
+        first_guess = first_guess[::-1,:,:]
+        
     if intermediate_steps:
         return first_guess, scaling_guesses
 

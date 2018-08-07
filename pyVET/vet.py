d1056355b834d064d7a8fc3d718322408ea5335e
Andres Perez Hortal <andresperezcba@gmail.com>
2018-08-07 17:43:58 -0400

    Add support for any images sizes, regarding the scaling procedure.
    Now if the number of sectors (previously called factors) do not divide
    the image exactly, a padding is added.

--- pyVET/vet.py
+++ pyVET/vet.py
@@ -18,7 +18,6 @@ in `Germann and Zawadzki (2002)`_.
 .. _`Germann and Zawadzki (2002)`:\
     http://dx.doi.org/10.1175/1520-0493(2002)130<2859:SDOTPO>2.0.CO;2
 
-
 The morphing and the cost functions are implemented in Cython and parallelized
 for performance.
 '''
@@ -26,7 +25,7 @@ for performance.
 from __future__ import (absolute_import, division,
                         print_function, unicode_literals)
 
-from numpy import float64
+
 import numpy
 from numpy.ma.core import MaskedArray
 from scipy.ndimage.interpolation import zoom
@@ -41,20 +40,44 @@ def round_int(scalar):
     """
     Round number to nearest integer. Returns and integer value.
     """
-    return int(round(scalar))
+    return int(numpy.round(scalar))
+
+
+def ceil_int(scalar):
+    """
+    Round number to nearest integer. Returns and integer value.
+    """
+    return int(numpy.ceil(scalar))
+
+
+def get_padding(dimension_size, sectors):
+
+    reminder = dimension_size % sectors
+
+    if reminder != 0:
+        pad = sectors - reminder
+        pad_before = pad // 2
+        if (pad % 2 == 0):
+            pad_after = pad_before
+        else:
+            pad_after = pad_before + 1
+
+        return pad_before, pad_after
+
+    return (0, 0)
 
 
 def morph(image, displacement):
     """
     Morph image by applying a displacement field (Warping) in the images
     coordinates.
-    
+
     Take caution that (rows, columns)=(i,j)==(y,x).
 
     See :py:func:`vet._vet._morph`.
     """
-    _image = numpy.asarray(image, dtype=float64, order='C')
-    _displacement = numpy.asarray(displacement, dtype=float64, order='C')
+    _image = numpy.asarray(image, dtype='float64', order='C')
+    _displacement = numpy.asarray(displacement, dtype='float64', order='C')
     return _morph(_image, _displacement)
 
 
@@ -169,9 +192,9 @@ def cost_function(sector_displacement_1d,
     return residuals + smoothness_penalty
 
 
-# TODO: add keywords for minimization options 
+# TODO: add keywords for minimization options
 def vet(input_images,
-        factors=[32, 16, 4, 2, 1],
+        sectors=[[32, 16, 4, 2, 1], [32, 16, 4, 2, 1]],
         smooth_gain=100,
         first_guess=None,
         intermediate_steps=False,
@@ -198,20 +221,20 @@ def vet(input_images,
     In order to find the minimum an scaling guess procedure is applied,
     from larger scales
     to a finer scale. This reduces the changes that the minimization procedure
-    converges to a local minima. The scaling guess is defined by the scaling
-    factors (see **factors** keyword).
+    converges to a local minimum. The scaling guess is defined by the scaling
+    sectors (see **sectors** keyword).
 
     The smoothness of the returned displacement field is controlled by the
     smoothness constrain gain (**smooth_gain** keyword).
 
     If a first guess is not given, zero displacements are used as first guess.
-    
+
     .. _MaskedArray: https://docs.scipy.org/doc/numpy/reference/\
         maskedarray.baseclass.html#numpy.ma.MaskedArray
-    
+
     .. _ndarray:\
     https://docs.scipy.org/doc/numpy/reference/generated/numpy.ndarray.html
-    
+
     Parameters
     ----------
 
@@ -224,17 +247,16 @@ def vet(input_images,
         The expected dimensions are (2,ny,nx).
         Be aware the the 2D images dimensions correspond to (y, x).
 
-    factors : list or array, optional
-        If dimension is 1, the same factors will be used both image dimensions
-        (x and y). If is 2D, the each row determines the factors of the
-        corresponding dimensions.
-        The factors denotes the number of sectors in each dimension used in
-        each scaling procedure.
+    sectors : list or array, optional
+        The number of sectors for each dimension used in the scaling procedure.
+        If dimension is 1, the same sectors will be used both image dimensions
+        (x and y). If is 2D, the each row determines the sectors of the
+        each dimension.
 
     smooth_gain : float, optional
         Smooth gain factor
 
-    first_guess : ndarray, optional_
+    first_guess : ndarray_, optional_
         If first_guess is not present zeros are used as first guess.
         The shape should be compatible with the input image.
         That is (2,nx,ny).
@@ -244,15 +266,15 @@ def vet(input_images,
         scaling procedure. False, by default.
 
     verbose : bool, optional
-        Verbosity enabled if True (default). 
+        Verbosity enabled if True (default).
 
     indexing : {‘xy’, ‘ij’}, optional
         Cartesian (‘xy’, default) or matrix (‘ij’) indexing of output.
         If ‘xy’ is selected, the displacement field dimensions correspond
         to (x,y). Otherwise, the displacements corresponds to indexes of the
         2D images (i,j).
-        
-    
+
+
     Returns
     -------
 
@@ -274,14 +296,11 @@ def vet(input_images,
     Radar Images.  Part I: Description of the Methodology.
     Mon. Wea. Rev., 130, 2859–2873,
     doi: 10.1175/1520-0493(2002)130<2859:SDOTPO>2.0.CO;2.
-
     '''
 
-    print("Running VET algorithm")
-    
     if indexing not in ['xy', 'ij']:
         raise ValueError("Valid values for `indexing` are 'xy' and 'ij'.")
-            
+
     if verbose:
         def debug_print(*args, **kwargs):
             print(*args, **kwargs)
@@ -289,98 +308,103 @@ def vet(input_images,
         def debug_print(*args, **kwargs):
             pass
 
+    debug_print("Running VET algorithm")
+
     input_images = numpy.array(input_images, dtype=numpy.float64)
 
     if (input_images.ndim != 3 or input_images.shape[0] != 2):
         raise ValueError("input_images dimension mismatch.\n" +
-                         "input_images.shape: %s\n" % str(input_images.shape)+
+                         "input_images.shape: %s\n" % str(input_images.shape) +
                          "(2, x, y ) dimensions expected.")
 
-
     # Get mask
-    if isinstance(input_images , MaskedArray):
+    if isinstance(input_images, MaskedArray):
         mask = numpy.ma.getmaskarray(input_images)
     else:
-        # Mask invalid data 
+        # Mask invalid data
         input_images = numpy.ma.masked_invalid(input_images)
         mask = numpy.ma.getmaskarray(input_images)
-    
+
     input_images[mask] = 0  # Remove any Nan from the raw data
-    
+
     # Create a 2D mask with the rigth data type for _vet
-    mask = numpy.asarray(numpy.any(mask, axis=0), dtype='int8') 
-    
-    template_image = numpy.asarray(input_images.data[0, :], dtype=numpy.float64)
+    mask = numpy.asarray(numpy.any(mask, axis=0), dtype='int8')
+
+    template_image = numpy.asarray(
+        input_images.data[0, :], dtype=numpy.float64)
     input_image = numpy.asarray(input_images.data[1, :], dtype=numpy.float64)
 
+    # Check that the sectors divide the domain
+    sectors = numpy.asarray(sectors, dtype=numpy.int)
 
-    # Check that the factors divide the domain
-    factors = numpy.asarray(factors, dtype=numpy.int)
+    if sectors.ndim == 1:
 
-    if factors.ndim == 1 and template_image.shape[0] == template_image.shape[1]:
-
-        new_factors = (numpy.zeros((2,) + factors.shape, dtype=numpy.int)
-                       + factors.reshape((1, factors.shape[0]))
+        new_sectors = (numpy.zeros((2,) + sectors.shape, dtype=numpy.int)
+                       + sectors.reshape((1, sectors.shape[0]))
                        )
-        factors = new_factors
+        sectors = new_sectors
+    elif sectors.ndim > 2 or sectors.ndim < 1:
+        raise ValueError("Incorrect sectors dimensions.\n",
+                         +"Only 1D or 2D arrays are supported to define"
+                         + "the number of sectors used in"
+                          + "the scaling procedure")
 
-    if factors.ndim != 2:
-        if factors.shape[0] != 2:
-            raise ValueError("Incorrect factors dimensions.\n",
-                             "factor should be a 1D if the input images"
-                             + " are square or 2D array otherwise")
-
-    # Check that the factors divide the domain
-    for i in range(factors.shape[1]):
-        if (template_image.shape[0] % factors[0, i]) > 0:
-            raise ValueError(
-                "The factor %d does not divide x dimension" % factors[0, i])
-
-        if (template_image.shape[1] % factors[1, i]) > 0:
-            raise ValueError(
-                "The factor %d does not divide y dimension" % factors[1, i])
-
-    # Sort factors in descending order
-    factors[0, :].sort()
-    factors[1, :].sort()
+    # Sort sectors in descending order
+    sectors[0, :].sort()
+    sectors[1, :].sort()
 
     # Prepare first guest
-    first_guess_shape = (2, int(factors[0, 0]), int(factors[1, 0]))
+    first_guess_shape = (2, int(sectors[0, 0]), int(sectors[1, 0]))
 
     if first_guess is None:
-        initial_guess = numpy.zeros(first_guess_shape, order='C')
+        first_guess = numpy.zeros(first_guess_shape, order='C')
     else:
         if first_guess.shape != first_guess_shape:
-            raise ValueError("The shape of the initial guess do not match " + 
-                             "the factors coarse resolution")
+            raise ValueError("The shape of the initial guess do not match " +
+                             "the sectors coarse resolution")
         else:
-            initial_guess = first_guess
+            first_guess = numpy.asarray(first_guess, order='C')
 
-    first_guess = None
-
-    _template_image = numpy.asarray(template_image, dtype='float64')
-    _input_image = numpy.asarray(input_image, dtype='float64')
+    template_image = numpy.asarray(template_image, dtype='float64')
+    input_image = numpy.asarray(input_image, dtype='float64')
 
     if intermediate_steps:
         scaling_guesses = list()
-        
-    for i in range(factors.shape[1]):
+
+    previous_sectors_in_i = sectors[0, 0]
+    previous_sectors_in_j = sectors[1, 0]
+
+    for n, (sectors_in_i, sectors_in_j) in enumerate(
+            zip(sectors[0, :], sectors[1, :])):
+
         # Minimize for each sector size
+        pad_i = get_padding(template_image.shape[0], sectors_in_i)
+        pad_j = get_padding(template_image.shape[1], sectors_in_j)
 
-        sector_shape = (round_int(template_image.shape[0] / factors[0, i]),
-                        round_int(template_image.shape[1] / factors[1, i]))
+        if (pad_i != (0, 0)) or (pad_j != (0, 0)):
 
-        debug_print("\nNumber of sectors: %s" % str(factors[:, i]))
+            _template_image = numpy.pad(template_image, (pad_i, pad_j), 'edge')
+            _input_image = numpy.pad(input_image, (pad_i, pad_j), 'edge')
+
+            if first_guess is None:
+                first_guess = numpy.pad(first_guess, (pad_i, pad_j), 'edge')
+        else:
+            _template_image = template_image
+            _input_image = input_image
+
+        sector_shape = (_template_image.shape[0] // sectors_in_i,
+                        _template_image.shape[1] // sectors_in_j)
+
+        debug_print("\nNumber of sectors: %d,%d" %
+                    (sectors_in_i, sectors_in_j))
+
         debug_print("Sector Shape:", sector_shape)
 
-        if first_guess is None:
-            first_guess = initial_guess
-        else:
-
+        if n > 0:
             first_guess = zoom(first_guess,
                                (1,
-                                factors[0, i] / factors[0, i - 1],
-                                factors[1, i] / factors[1, i - 1]),
+                                sectors_in_i / previous_sectors_in_i,
+                                sectors_in_j / previous_sectors_in_j),
                                order=1, mode='nearest')
 
         debug_print("Minimizing")
@@ -388,7 +412,8 @@ def vet(input_images,
         result = minimize(cost_function,
                           first_guess.flatten(),
                           args=(_template_image, _input_image,
-                                factors[:, i], mask,
+                                (sectors_in_i, sectors_in_j),
+                                mask,
                                 smooth_gain),
                           method='CG',
                           options={'eps': 0.1, 'gtol': 0.1,
@@ -400,25 +425,36 @@ def vet(input_images,
         if verbose:
             cost_function(result.x,
                           _template_image, _input_image,
-                          factors[:, i], mask,
+                          (sectors_in_i, sectors_in_j),
+                          mask,
                           smooth_gain,
                           debug=True)
 
         if first_guess is None:
             if indexing == 'xy':
-                scaling_guesses.append(first_guess[::-1,:,:])
+                scaling_guesses.append(first_guess[::-1, :, :])
             else:
                 scaling_guesses.append(first_guess)
 
+        previous_sectors_in_i = sectors_in_i
+        previous_sectors_in_j = sectors_in_j
+
     first_guess = zoom(first_guess,
                        (1,
-                        _template_image.shape[0] / first_guess.shape[1],
-                        _template_image.shape[1] / first_guess.shape[2]),
+                        _template_image.shape[0] / sectors_in_i,
+                        _template_image.shape[1] / sectors_in_j),
                        order=1, mode='nearest')
-    
+
+    # Remove the extra padding if any
+    ni = _template_image.shape[0]
+    nj = _template_image.shape[1]
+
+    first_guess = first_guess[pad_i[0]:ni - pad_i[1],
+                              pad_j[0]:nj - pad_j[1]]
+
     if indexing == 'xy':
-        first_guess = first_guess[::-1,:,:]
-        
+        first_guess = first_guess[::-1, :, :]
+
     if intermediate_steps:
         return first_guess, scaling_guesses
 
